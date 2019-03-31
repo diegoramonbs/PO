d9fee66e865a8db084c2046dd59da1d26567f29a
diegoramonbs <37641499+diegoramonbs@users.noreply.github.com>
2019-03-31 19:09:52 -0300

    Update test.py

--- test.py
+++ test.py
@@ -6,16 +6,20 @@
 
 from simplex import simplex
 
+# Exemplo padrão do slide  (Valor ótimo: 36.0)
+A = np.array([[1,0],[0,2],[3,2]])
+b = np.array([4,12,18])
+c = np.array([3, 5])
+s = np.array(["<=", "<=", "<="])
 
-# Exemplo com restrinções do tipo igualdade 
+# Exemplo com restrições do tipo igualdade  (Valor ótimo: 36.0)
 A = np.array([[1,0],[0,2],[3,2]])
 b = np.array([4,12,18])
 c = np.array([3,5])
 s = np.array(["<=", "<=", "="])
 simplex(A, b, c, s, "min", verbose=False)
 
-
-# Exemplo de problema ilimitado
+# Exemplo de problema ilimitado (Exception)
 A = np.array([[1,-1],[-1,1]])
 b = np.array([4, 4])
 c = np.array([-1, -1])
@@ -23,8 +27,7 @@ s = np.array(["<=", "<="])
 simplex(A, b, c, s, "min", verbose=False)
 
 
-
-# Exemplo mais geral, sendo um problema de minimização e com restrições de todos os tipos 
+# Exemplo mais geral, sendo um problema de minimização e com restrições de todos os tipos (Valor ótimo: -5.25)
 A = np.array([[0.3,0.1],[0.5,0.5],[0.6,0.4]])
 b = np.array([2.7, 6, 6])
 c = np.array([0.4, 0.5])
